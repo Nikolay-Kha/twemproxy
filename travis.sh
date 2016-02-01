73fbdc3f6c02a1438472ab4ccbcc29ed1c26d284
Nikolay Khabarov <2xl@mail.ru>
2016-02-01 19:52:08 +0300

    update travis

--- travis.sh
+++ travis.sh
@@ -3,18 +3,6 @@
 #author : ning
 #date   : 2014-05-10 16:54:43
 
-#install deps if we are in travis
-if [ -n "$TRAVIS" ]; then
-    sudo apt-get install socat
-
-    #python libs
-    sudo pip install redis
-    sudo pip install nose
-
-    sudo pip install git+https://github.com/andymccurdy/redis-py.git@2.10.3
-    sudo pip install git+https://github.com/idning/python-memcached.git#egg=memcache
-fi 
-
 #build twemproxy
 CFLAGS="-ggdb3 -O0" autoreconf -fvi && ./configure --enable-debug=log && make 
 
